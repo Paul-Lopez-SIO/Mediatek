2446eaf90ee06d46075013b947931e373bb671fc
paul <paul@PAULLOPEZ>
2025-06-05 19:42:27 +0200

    Inclusion base de données

--- Mediatek/mediatek.sql
+++ Mediatek/mediatek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 05 juin 2025 à 17:39
+-- Généré le : jeu. 05 juin 2025 à 17:41
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `mediatek`
 --
+CREATE DATABASE IF NOT EXISTS `mediatek` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mediatek`;
 
 -- --------------------------------------------------------
 
